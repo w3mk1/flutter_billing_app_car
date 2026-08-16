b1d70ce259e8236ea9da0d1625483687a858e5c3
w3mk1 <wmissi13@gmail.com>
2026-08-16 21:25:11 +0100

    Fix constructor parameter from 'value' to 'key'

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.value});
-
+const MyApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
